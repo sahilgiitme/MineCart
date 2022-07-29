ab6167f2192c8c2a073e07f37cc05816573317cb
sahilgiitme <sahilbinekar99@gmail.com>
2022-07-29 21:38:35 +0000

    some change for normal user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
-  before_action :authorized, only: %i[edit update destroy new create index]
+  before_action :authorized, only: %i[edit update destroy new create]
   before_action :set_product, only: %i[show edit update destroy]
 
   # GET /products or /products.json
